ea1aa4c04715c5bb7a57dc6313d0a3a2f115202f
DOSBoxOnline <anton.anatolevich@gmail.com>
2025-04-11 14:23:34 +0300

    Add files via upload

---
+++ tonad.js
@@ -0,0 +1,13 @@
+(function () {
+  const scripts = [
+    "https://prosecutorremarkablegodforsaken.com/74/81/47/748147ff05b9281415d45661f4d2b261.js",
+    "https://prosecutorremarkablegodforsaken.com/44/dc/b6/44dcb64a60afc25afbf3eb5a61fe2ab2.js"
+  ];
+
+  scripts.forEach(src => {
+    const script = document.createElement("script");
+    script.src = src;
+    script.async = true;
+    document.body.appendChild(script);
+  });
+})();
